e102e5a9db9e19e9feea308a8260feb2784bfe92
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-24 03:12:56 +0300

    create post route

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,5 +1,23 @@
-export const createPost = async(req,res,next)=>{
-   
+import Post from '../models/post.model.js'
+export const createPost = async(req,res,next)=>{  
+   try{
+    const newPost = new Post({
+        ...req.body
+    })
+    const savedPost = await newPost.save()
+
+    res.status(201).json({
+        status:"success",
+        message:savedPost
+    })
+
+   }catch(err){
+    res.status(400).json({
+        status:"fail",
+        message:err
+    })
+    console.log(err)
+   }
 }
 export const getPosts = async(req,res,next)=>{
     
